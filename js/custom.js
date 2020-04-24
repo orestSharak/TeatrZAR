be76ea720372e868fa98793ef963ed0ed5fcefe6
sharakOrest <sharak.orest@gmail.com>
2020-04-24 11:26:02 +0200

    index change jquery migration for slick slider

--- js/custom.js
+++ js/custom.js
@@ -191,18 +191,20 @@ new simpleParallax(video, {
 
     // slick slider
 
-
-$('.slider').slick({
-    autoplay: true,
-    speed: 800,
-    arrows: false,
-    dots: true,
-    pauseOnHover:false,
-    pauseOnFocus:false,
-    pauseOnDotsHover:false,
-    fade:false,
-    autoplaySpeed: 6000,
-}).slickAnimation();
+$(document).ready(function() {
+    $('.slider').slick({
+        autoplay: true,
+        speed: 800,
+        arrows: false,
+        dots: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        pauseOnDotsHover: false,
+        fade: false,
+        autoplaySpeed: 6000,
+        mobileFirst: true
+    }).slickAnimation();
+});
 
 
 
